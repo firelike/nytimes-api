761c33a79b3e83f137a60674600d36c1ff84c652
Plamen Garkov <plamen@flexiebooks.com>
2016-12-18 14:27:09 -0800

    fixing bug with the factory

--- src/Validator/Factory/BooksServiceRequestValidatorFactory.php
+++ src/Validator/Factory/BooksServiceRequestValidatorFactory.php
@@ -11,8 +11,8 @@ class BooksServiceRequestValidatorFactory implements FactoryInterface
     public function __invoke(ContainerInterface $sm, $requestedName, array $options = null)
     {
         $validator = new BooksServiceRequestValidator();
-        $validator->setSortOrderValidator($sm->get('Firelike\ShareASale\Validator\SortOrderValidator'));
-        $validator->setOffsetValidator($sm->get('Firelike\ShareASale\Validator\OffsetValidator'));
+        $validator->setSortOrderValidator($sm->get('Firelike\NYTimes\Validator\SortOrderValidator'));
+        $validator->setOffsetValidator($sm->get('Firelike\NYTimes\Validator\OffsetValidator'));
         return $validator;
     }
 
